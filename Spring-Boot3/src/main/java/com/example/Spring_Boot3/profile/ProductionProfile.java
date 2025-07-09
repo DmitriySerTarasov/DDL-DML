30854f9f0c5e4ef84d34beb114cb3ea3dc08d099
Дмитрий <tds1991j@gmail.com>
2025-07-09 14:05:38 +0700

    Delete Spring-Boot3 directory

--- Spring-Boot3/src/main/java/com/example/Spring_Boot3/profile/ProductionProfile.java
+++
@@ -1,8 +0,0 @@
-package com.example.Spring_Boot3.profile;
-
-public class ProductionProfile implements SystemProfile{
-    @Override
-    public String getProfile() {
-        return "Current profile is production";
-    }
-}
